bd271eed0bc3a020da25a915cda78349a2e970bd
ivy <ivynjoki2024@gmail.com>
2025-03-13 07:27:05 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+describe('index.js', function (portfolio) {
+    // there's stuff in here, too
+  });
+  
+
+  it('is set as portfolio', function () {
+    // tests are here
+  });
+  let companyName = 'portfolio';
+    let mostProfitableNeighborhood = 'Chelsea';it('is set as portfolio', function () {
+      expect(companyName).to.equal('portfolio');
+    });
+    it('is set as portfolio', function () {
+      expect(portfolio).to.equal('portfolio');
+    });
+    let companyName = 'portfolio';
+
+      
